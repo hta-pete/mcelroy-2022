a0ffeb26eb01aa27361b35e80fdf61faf7070d6f
hta-pete <pkatsaboulas@hightoweragency.com>
2022-06-03 09:49:36 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(function(){
+
+	var $window       = $(window);
+  var raf           = requestAnimationFrame;
+  var lastScrollTop = $window.scrollTop();
+  var scrollTop;
+
+  $window.on("load", function(){
+    scrollTop = $window.scrollTop();
+    if( scrollTop > 90 ){
+      $('header').addClass('active');
+    } else{
+      $('header').removeClass('active');
+    }  
+  });
+
+  if (raf) {
+    loop();
+  }
+
+  function loop() {
+
+    scrollTop = $window.scrollTop();
+    var y         = (scrollTop > lastScrollTop) ? 'down' : ((scrollTop === lastScrollTop) ? 'none' : 'up');
+
+    if (lastScrollTop === scrollTop) {
+        raf(loop);
+        return;
+    } else {
+        lastScrollTop = scrollTop;
+        raf(loop);
+    }
+ 
+    lastScrollTop = scrollTop;
+
+    if( scrollTop > 90 ){
+      $('header').addClass('active');
+    } else{
+      $('header').removeClass('active');
+    }  
+
+  }
+ 
+});
+
+
+
+
