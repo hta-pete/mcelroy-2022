c74c5a6d1669c9a7deed69df61a3753425d647cc
hta-pete <pkatsaboulas@hightoweragency.com>
2022-07-13 10:50:33 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -43,11 +43,35 @@ $(function(){
 
 
   $(".mobile-menu-btn").on("click", function(){
-
     $(this).toggleClass("active");
     $(".mobile-nav").toggleClass("show");
-
   });
+
+
+  // Modals
+  $('.close-modal-btn, .close-modal-link').on('click', function(){
+    $('.modal-window').fadeOut('fast', function(){
+      if( $('#video-modal') ){
+        $('#video-modal iframe').attr('src', '');
+      }
+    });
+  }); 
+
+  $('.modal-window').on('click', function(e) {
+    if (!$(e.target).closest('.modal').length){
+      $('.modal-window').fadeOut('fast', function(){
+        if( $('#video-modal') ){
+          $('#video-modal iframe').attr('src', '');
+        }
+      });
+    }
+  });
+
+  $('.btn-watch-video').on('click', function(){
+   var videoURL = $(this).data('url');
+   $('#video-modal iframe').attr('src', videoURL);
+   $('#video-modal').fadeIn('fast');
+ });
  
 });
 
